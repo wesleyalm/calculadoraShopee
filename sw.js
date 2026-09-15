fcd79deea1589f5095f892b5e3e34578c5262486
wesleyalm <140283294+wesleyalm@users.noreply.github.com>
2026-09-15 11:02:06 -0300

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('mwfix-calc-v1').then((cache) => {
+      return cache.addAll(['./index.html']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
